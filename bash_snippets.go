4c0ecc2a42eeed3ce203c65ce18862e17f9d1026
Konstantin Semenov <ksemenov@pivotal.io>
2017-05-21 17:09:44 +0100

    Sore STDIN in a scalar variable rather than an array

--- bash_snippets.go
+++ bash_snippets.go
@@ -3,23 +3,23 @@ package gobmock
 const (
 	scriptStart    = "%[1]s() {\n"
 	stubDefinition = `
-          while read -t0.05; do
+          while read -r -t0.1; do
             :
           done
   	`
 	spyDefinition = `
-          local in_line_count=0
-          declare -a in_lines
+          local in_lines
           while read -r -t0.1; do
-            in_lines[in_line_count]="$REPLY"
-            in_line_count=$((in_line_count + 1))
+            in_lines="${in_lines}${REPLY}
+"
           done
           callCounter=$((callCounter + 1))
-          echo "<$callCounter> %[1]s $*" > /dev/fd/2
-          if [ ${in_line_count} -gt 0 ]; then
-            echo "<$callCounter received> input:" > /dev/fd/2
-            printf '%%s\n' "${in_lines[@]}" > /dev/fd/2
-            echo "<$callCounter end received>" > /dev/fd/2
+          echo "<${callCounter}> %[1]s $*" > /dev/fd/2
+          if [ -n "${in_lines}" ]; then
+            in_lines=${in_lines::-1}
+            echo "<${callCounter} received> input:" > /dev/fd/2
+            echo -n "${in_lines}" > /dev/fd/2
+            echo "<${callCounter} end received>" > /dev/fd/2
           fi
         `
 	mockDefinition   = "%[2]s\n"
@@ -27,8 +27,8 @@ const (
 	exportDefinition = "export -f %s\n"
 
 	callThroughDefinition = `
-	  if [ ${in_line_count} -gt 0 ]; then
-	    echo $(IFS=$'\n'; echo ${in_lines[*]}) | $(which %[1]s) "$@"
+	  if [ -n "${in_lines}" ]; then
+	    echo -n "${in_lines}" | $(which %[1]s) "$@"
 	  else
 	    $(which %[1]s) "$@"
 	  fi
